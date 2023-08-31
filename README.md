9217b17a13ce8b9ce3cab7387c5e552a11e48303
jixatos <j.gody2004@gmail.com>
2023-08-31 11:06:03 -0300

    Listagem dos produtos em tabela realizada

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+##Mapeando ações com o Router-DOM
+- Criar um módulo com uma lista de objetos. Um arquivo de nome ListaProdutos que vai conter uma lista de objetos com os produtos que serão exibidos na tela. Cada produto deve ter um id, nome, descrição e preço
+- Listar os produtos na tela usando o método map() para percorrera lsita de produtos e exibir cada um deles na tela. Os produtos devem ser exibidos em uma tabela comonente em Produtos.jsx
+- Criando a coluna de editar e excluir. Crie uma coluna na tabela para editar e excluir os produtos. Para isso, crie dois links, um para editar e outro para excluir. Vamos utilizar o id dos produtos como parâmetro.
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,8 +1,37 @@
+import { ListaProdutos } from "../components/ListaProdutos"
+
 export default function Produto(){
 
     return(
         <>
-            <h1>Produtos</h1>
+            <h1>Produtos Informáticos - FIAPO</h1>
+            <table>
+            <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>NOME</th>
+                  <th>DESCRIÇÃO</th>
+                  <th>PREÇO</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {ListaProdutos.map((produto, indice)=>(
+                    <tr key={indice}>
+                        <td>{produto.id}</td>
+                        <td>{produto.nome}</td>
+                        <td>{produto.desc}</td>
+                        <td>{produto.preco}</td>
+                    </tr>
+                ))}
+              </tbody>
+
+              <tfoot>
+                <tr>
+                    <td colSpan={3}>PRODUTOS</td>
+                </tr>
+              </tfoot>
+            </table>
         </>
     )
 }
