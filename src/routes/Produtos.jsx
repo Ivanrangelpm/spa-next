8f34fff93a4d230ce718ba2160ee2ee4073a98c0
jixatos <j.gody2004@gmail.com>
2023-09-13 12:17:22 -0300

    Estilização das rotas com 'paginas'.module.css

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import {AiFillEdit as EditObj} from "react-icons/ai";
 import {BsFillTrashFill as BinObj} from "react-icons/bs";
+import estilos from "./Produtos.module.css";
 
 export default function Produto(){
 
@@ -10,25 +11,25 @@ export default function Produto(){
     return(
         <>
             <h1>Produtos Informáticos - FIAPO</h1>
-            <table>
+            <table className={estilos.tblEstilo}>
             <thead>
                 <tr>
-                  <th>ID</th>
-                  <th>NOME</th>
-                  <th>DESCRIÇÃO</th>
-                  <th>PREÇO</th>
-                  <th>Editar/Excluir</th>
+                  <th className={estilos.tblHeader}>ID</th>
+                  <th className={estilos.tblHeader}>NOME</th>
+                  <th className={estilos.tblHeader}>DESCRIÇÃO</th>
+                  <th className={estilos.tblHeader}>PREÇO</th>
+                  <th className={estilos.tblHeader}>Editar/Excluir</th>
                 </tr>
               </thead>
 
               <tbody>
                 {ListaProdutos.map((produto, indice)=>(
-                    <tr key={indice}>
-                        <td>{produto.id}</td>
-                        <td>{produto.nome}</td>
-                        <td>{produto.desc}</td>
-                        <td>{produto.preco}</td>
-                        <td> <Link to={`/editar/produtos/${produto.id}`}> <EditObj/></Link> |
+                    <tr key={indice} className={estilos.tblLine}>
+                        <td className={estilos.tblData}>{produto.id}</td>
+                        <td className={estilos.tblData}>{produto.nome}</td>
+                        <td className={estilos.tblData}>{produto.desc}</td>
+                        <td className={estilos.tblData}>{produto.preco}</td>
+                        <td className={estilos.tblData}> <Link to={`/editar/produtos/${produto.id}`}> <EditObj/></Link> |
                         <Link to={`/excluir/produtos/${produto.id}`}><BinObj/></Link></td>
                     </tr>
                 ))}
