d05358083394f7d0f015a525b9f96dae33c26365
jixatos <j.gody2004@gmail.com>
2023-09-21 12:29:41 -0300

    Criando bloco fetch para requisição e utilizando o useEffect para controlar o  fluxo de dados.

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,57 +1,78 @@
 import { Link } from "react-router-dom";
-import { ListaProdutos } from "../../components/ListaProdutos";
-import {AiFillEdit as EditObj} from "react-icons/ai";
-import {BsFillTrashFill as BinObj} from "react-icons/bs";
+import { AiFillEdit as EditObj } from "react-icons/ai";
+import { BsFillTrashFill as BinObj } from "react-icons/bs";
 import estilos from "./index.module.css";
+import { useEffect, useState } from "react";
 
-export default function Produto(){
+export default function Produto() {
+  document.title = "Lista de Produtos";
 
-  document.title = "Lista de Produtos"
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
 
-  //Criando o bloco de reequisição dos dados utilizando o fetch com promises:
-  
+  useEffect(() => {
+    //Criando o bloco de reequisição dos dados utilizando o fetch com promises:
+    fetch("http://localhost:5000/produtos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setListaProdutosLocal(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return(
-        <>
-          <h1>Produtos Informáticos - FIAPO</h1>
-          <div>
-            <Link to={"/adicionar/produtos"}>
-              <button>Adicionar novo produto</button>
-            </Link>
-          </div>
-          <table className={estilos.tblEstilo}>
-          <thead>
-              <tr>
-                <th>ID</th>
-                <th>NOME</th>
-                <th>DESCRIÇÃO</th>
-                <th>PREÇO</th>
-                <th>IMAGEM</th>
-                <th>Editar/Excluir</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ListaProdutos.map((produto, indice)=>(
-                  <tr key={indice} className={estilos.tblLine}>
-                      <td>{produto.id}</td>
-                      <td>{produto.nome}</td>
-                      <td>{produto.desc}</td>
-                      <td>{produto.preco}</td>
-                      <td><img src={produto.img} alt={produto.desc} /></td>
-                      <td>
-                        <Link to={`/editar/produtos/${produto.id}`}> <EditObj/></Link> |
-                        <Link to={`/excluir/produtos/${produto.id}`}><BinObj/></Link>
-                      </td>
-                  </tr>
-              ))}
-            </tbody>
+  return (
+    <>
+      <h1>Produtos Informáticos - FIAPO</h1>
+      <div>
+        <Link to={"/adicionar/produtos"}>
+          <button>Adicionar novo produto</button>
+        </Link>
+      </div>
+      <table className={estilos.tblEstilo}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+            <th>IMAGEM</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProdutosLocal.map((produto, indice) => (
+            <tr key={indice} className={estilos.tblLine}>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.desc}</td>
+              <td>{produto.preco}</td>
+              <td>
+                <img src={produto.img} alt={produto.desc} />
+              </td>
+              <td>
+                <Link to={`/editar/produtos/${produto.id}`}>
+                  {" "}
+                  <EditObj />
+                </Link>{" "}
+                |
+                <Link to={`/excluir/produtos/${produto.id}`}>
+                  <BinObj />
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
 
-            <tfoot>
-              <tr>
-                  <td colSpan={6}>PRODUTOS</td>
-              </tr>
-            </tfoot>
-          </table>
-        </>
-    )
+        <tfoot>
+          <tr>
+            <td colSpan={6}>PRODUTOS</td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
+  );
 }
