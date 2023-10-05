1a18514fa0664c44c4a6b482a14bfa39f01e021a
jixatos <j.gody2004@gmail.com>
2023-10-05 11:38:01 -0300

    Estilização do cabecário com SASS e condicional ternário

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
-import 
+import { Link, useLocation } from "react-router-dom";
+import "./Cabecalho.scss"
 
 export default function Cabecalho(){
+
+    const rotaAtual = useLocation();
+    console.log("ROTA ATUAL: " + rotaAtual.pathname)
 
     return(
         <header className="cabecalho">
             <nav>
             {/* <h1>Vite + React / Coded by - rm551408</h1> */}
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/produtos">Produtos</Link></li>
+                    <li><Link to="/" className={rotaAtual.pathname == "/" ? "active" : ""}>Home</Link></li>
+                    <li><Link to="/produtos" className={rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link></li>
                 </ul>
             </nav>
         </header>
