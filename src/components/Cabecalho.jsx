ce9b8974e58a4433459ce5aa7b20b8245651ad64
jixatos <j.gody2004@gmail.com>
2023-10-05 10:46:23 -0300

    Instalação do "SASS"

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 export default function Cabecalho(){
 
     return(
-        <header>
-            <h1>Vite + React / Coded by - rm551408</h1>
+        <header className="cabecalho">
+            <nav>
+            {/* <h1>Vite + React / Coded by - rm551408</h1> */}
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/produtos">Produtos</Link></li>
                 </ul>
+            </nav>
         </header>
     );
 }
